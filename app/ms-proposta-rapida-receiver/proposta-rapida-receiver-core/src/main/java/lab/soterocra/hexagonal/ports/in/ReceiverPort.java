2a057f7a7ae5b1acc0e6c77c6ac499bd18fef6dc
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2021-06-23 18:02:35 -0300

    :fire: Remove ms-proposta-rapida-receiver

--- app/ms-proposta-rapida-receiver/proposta-rapida-receiver-core/src/main/java/lab/soterocra/hexagonal/ports/in/ReceiverPort.java
+++
@@ -1,7 +0,0 @@
-package lab.soterocra.hexagonal.ports.in;
-
-import lab.soterocra.hexagonal.domain.model.Message;
-
-public interface ReceiverPort {
-    void hook(Message message) throws Exception;
-}
--- app/ms-proposta-rapida-receiver/proposta-rapida-receiver-core/src/main/java/lab/soterocra/hexagonal/ports/out/PostMessagePort.java
+++
@@ -1,7 +0,0 @@
-package lab.soterocra.hexagonal.ports.out;
-
-import lab.soterocra.hexagonal.domain.model.Message;
-
-public interface PostMessagePort {
-    void sendMessage(Message message);
-}
